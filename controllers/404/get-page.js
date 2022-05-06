0fedc53967ddf0dd1b17c964faf3239dd8d7eb2f
Bardita Andrei <bmihaiandrei@gmail.com>
2022-05-06 23:26:04 +0300

    handle 404

--- controllers/404/get-page.js
+++ controllers/404/get-page.js
@@ -1,5 +1,6 @@
 module.exports = (req, res) => {
-  res.render('pagini/404');
-
-  res.status(404);
+  res.render('pagini' + req.url, (err, rezRandare) => {
+    if (err && err.message.includes('Failed to lookup')) res.status(404).render('pagini/404');
+    else res.send(rezRandare);
+  });
 };
