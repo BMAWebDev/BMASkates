9aeb451754c9831f12d7ee2a855f4dd800abd725
Bardita Andrei <bmihaiandrei@gmail.com>
2022-05-07 15:52:53 +0300

    pagina contact

--- router.js
+++ router.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
-const { errorHandler, notFound } = require('./middleware');
+const { errorHandler } = require('./middleware');
 const { Error404 } = require('./controllers');
-const { acasa } = require('./routes');
+const { acasa, contact } = require('./routes');
 
 const router = Router();
 module.exports = router;
 
 // utilizeaza instantele definite
 router.use(acasa);
+router.use(contact);
 
 // ruta pentru 404, atunci cand nu gaseste nicio alta ruta de mai sus
 router.all('*', Error404.getPage);
