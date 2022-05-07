711118511f7a92f986094fff1158e846667d02a7
Bardita Andrei <bmihaiandrei@gmail.com>
2022-05-07 17:44:28 +0300

    stilizare tabel

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
 const Acasa = require('./acasa');
-const Error404 = require('./404');
 const Contact = require('./contact');
 const Restul = require('./restul');
 
-module.exports = { Acasa, Error404, Contact, Restul };
+module.exports = { Acasa, Contact, Restul };
