e9279537c7581ccfc621acd83d5aec07cc608a6a
Bardita Andrei <bmihaiandrei@gmail.com>
2022-05-14 21:14:38 +0300

    locatie + time

--- controllers/acasa/get-page.js
+++ controllers/acasa/get-page.js
@@ -1,8 +1,15 @@
 const { getIP } = require('../../middleware');
+const request = require('request');
 
 module.exports = (req, res) => {
-  res.render('pagini/index', {
-    page: '/index',
-    ip: getIP(req),
+  request(req.ip, (err, resRequest, body) => {
+    const locatie = `${JSON.parse(body).geobytesregion}, ${JSON.parse(body).geobytescountry}`;
+
+    res.render('pagini/index', {
+      page: '/index',
+      ip: getIP(req),
+      serverTime: new Date().toLocaleTimeString(),
+      locatie,
+    });
   });
 };
