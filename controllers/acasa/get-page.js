954d3a4b57df39328ee316e343700a4ad9e60e3f
Bardita Andrei <bmihaiandrei@gmail.com>
2022-05-14 22:09:38 +0300

    locatie + fix homepage

--- controllers/acasa/get-page.js
+++ controllers/acasa/get-page.js
@@ -2,12 +2,13 @@ const { getIP } = require('../../middleware');
 const request = require('request');
 
 module.exports = (req, res) => {
-  request(req.ip, (err, resRequest, body) => {
+  const ip = getIP(req);
+  request(`https://secure.geobytes.com/GetCityDetails?key=7c756203dbb38590a66e01a5a3e1ad96&fqcn=${ip}`, (err, resRequest, body) => {
     const locatie = `${JSON.parse(body).geobytesregion}, ${JSON.parse(body).geobytescountry}`;
 
     res.render('pagini/index', {
       page: '/index',
-      ip: getIP(req),
+      ip,
       serverTime: new Date().toLocaleTimeString(),
       locatie,
     });
